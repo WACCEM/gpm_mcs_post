e20170e02bf69bc1e94cc26166826db5a51d5bab
Zhe Feng <zhe.feng@pnnl.gov>
2020-12-04 17:42:46 -0800

    Updated Hovmoller code to take lat/lon boundary from input at runtime.

--- script/run_mcs_monthly_rainhov.sh
+++ script/run_mcs_monthly_rainhov.sh
@@ -2,4 +2,4 @@
 
 conda activate /global/homes/f/feng045/envs/py37
 
-python /global/homes/f/feng045/program/gpm_mcs_post/calc_imerg_mcs_monthly_preciphov_single.py $1 $2 $3 $4 $5
+python /global/homes/f/feng045/program/gpm_mcs_post/calc_imerg_mcs_monthly_preciphov_single.py $1 $2 $3 $4 $5 $6 $7 $8 $9
